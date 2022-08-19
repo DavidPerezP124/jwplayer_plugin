ec050bc60e433e67f0b49815e69d74b6c0c00433
David Perez <davidpp124@gmail.com>
2022-08-19 17:04:50 -0500

    env, license

---
+++ player_demo/lib/src/utils/license_util.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class GetLicense {
+  static final TargetPlatform _platform = defaultTargetPlatform;
+
+  static String getLicense() {
+    switch (_platform) {
+      case TargetPlatform.android:
+        return dotenv.get("ANDROID_LICENSE");
+      case TargetPlatform.iOS:
+        return dotenv.get("IOS_LICENSE");
+      default:
+        throw dotenv.get("WEB_LICENSE");
+    }
+  }
+}
